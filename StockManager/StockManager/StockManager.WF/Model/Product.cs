c289e378e6eb1970f40f15e1f59d7f02729c96de
Allan Blanchard <a.blanchard@iia-laval.fr>
2020-02-26 10:26:49 +0100

    Starting project StockManager

---
+++ StockManager/StockManager/StockManager.WF/Model/Product.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManager.WF.Model
+{
+    class Product
+    {
+		#region Proprietes
+		/// <summary>
+		/// Identifiant du produit
+		/// </summary>
+		private int _Identifier;
+
+		/// <summary>
+		/// Nom du produit
+		/// </summary>
+		private string _Nom;
+
+		/// <summary>
+		/// Reference du produit
+		/// </summary>
+		private string _Reference;
+
+		/// <summary>
+		/// Prix du produit
+		/// </summary>
+		private double _Price;
+
+		/// <summary>
+		/// Description du produit
+		/// </summary>
+		private string _Description;
+
+		/// <summary>
+		/// Quantité stockée du produit
+		/// </summary>
+		private int _StockedQuantity;
+
+
+		private ProductCategory _ProductCategory;
+		#endregion
+
+		#region Attributes
+
+		/// <summary>
+		/// Identifiant du produit
+		/// </summary>
+		public int Identifier
+		{
+			get { return _Identifier; }
+			set { _Identifier = value; }
+		}
+
+
+		/// <summary>
+		/// Nom du produit
+		/// </summary>
+		public string Nom
+		{
+			get { return _Nom; }
+			set { _Nom = value; }
+		}
+
+
+		/// <summary>
+		/// Reference du produit
+		/// </summary>
+		public string Reference
+		{
+			get { return _Reference; }
+			set { _Reference = value; }
+		}
+
+
+		/// <summary>
+		/// Prix du produit
+		/// </summary>
+		public double Price
+		{
+			get { return _Price; }
+			set { _Price = value; }
+		}
+
+
+		/// <summary>
+		/// Description du produit
+		/// </summary>
+		public string Description
+		{
+			get { return _Description; }
+			set { _Description = value; }
+		}
+
+
+		/// <summary>
+		/// Quantité stockée du produit
+		/// </summary>
+		public int StockedQuantity
+		{
+			get { return _StockedQuantity; }
+			set { _StockedQuantity = value; }
+		}
+
+
+
+
+
+		public ProductCategory ContactType
+		{
+			get { return _ProductCategory; }
+			set { _ProductCategory = value; }
+		}
+		#endregion
+
+
+
+	}
+}
---
+++ StockManager/StockManager/StockManager.WF/Model/ProductCategory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManager.WF.Model
+{
+    class ProductCategory
+    {
+
+		#region Properties
+		/// <summary>
+		/// Identifiant de la categorie du produit
+		/// </summary>
+		private int _Identifier;
+
+		/// <summary>
+		/// Label de la categorie du produit
+		/// </summary>
+		private string _Label;
+		#endregion
+
+		#region Attributes
+		/// <summary>
+		/// Identifiant de la categorie du produit
+		/// </summary>
+		public int Identifier
+		{
+			get { return _Identifier; }
+			set { _Identifier = value; }
+		}
+
+		/// <summary>
+		/// Label de la categorie du produit
+		/// </summary>
+		public string Label
+		{
+			get { return _Label; }
+			set { _Label = value; }
+		}
+		#endregion
+	}
+}
